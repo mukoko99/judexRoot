7d6a2b1da1a42338faea12377d5346ad293df192
Mubarak Ajibola Odufade <mubarakodufade@gmail.com>
2022-12-02 12:21:26 -0500

    Updated convict to include defendants

--- cases/models.py
+++ cases/models.py
@@ -78,13 +78,7 @@ class Prison(models.Model):
 
 class Convict(models.Model):
     DIN = models.CharField('Departmental Identification Number', max_length=7)
-    firstName = models.CharField('First Name', max_length=32)
-    lastName = models.CharField('Last Name', max_length=32)
-    middleName = models.CharField('Middle Name', max_length=32, default=' ')    
-    DOB = models.DateField()
-    Age = models.IntegerField('Age')
-    gender = models.CharField(max_length=1)
-    race = models.CharField(max_length = 32)
+    defendant = models.ForeignKey(Defendant, on_delete=models.CASCADE, blank=True, null=True)
     status = models.CharField(max_length=32)
     facility = models.ForeignKey(Prison, on_delete=models.SET_NULL, null=True, blank=True)
     charges = models.ManyToManyField(Charge)
